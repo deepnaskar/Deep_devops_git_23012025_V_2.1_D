31a59cad6416c8a9b213a0a35b043c4ff7e0d215
deepnaskar <naskarr.deep96@gmail.com>
2025-01-23 19:01:51 +0530

    Delete run_java_app.sh

--- run_java_app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "********************************************"
-echo "Please run this script as the 'ubuntu' user"
-echo "or you will face authorization errors."
-echo "********************************************"
-
-# Run the JAR file
-java -jar /usr/Deep_devops_git_23012025_V_2.1_D/target/Deep_devops_sr_dev_first.jar
